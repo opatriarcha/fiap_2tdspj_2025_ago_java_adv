b9058bcbfa0d099ef1af3e44571f3cab76be0789
Orlando <orlando.patriarcha@gmail.com>
2026-05-12 08:42:15 -0300

    iniido projeto mcp tooling

---
+++ lazyGrader/src/main/java/br/com/fiap/javaadv/lazyGrader/LazyGraderApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.javaadv.lazyGrader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazyGraderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazyGraderApplication.class, args);
+	}
+
+}
